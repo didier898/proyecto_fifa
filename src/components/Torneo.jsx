98e5c19396fca3afc240c65eec179f828c756c30
Didier <didier002010@gmail.com>
2023-07-27 18:05:12 -0500

    pagina de sorteos casi lista

--- src/components/Torneo.jsx
+++ src/components/Torneo.jsx
@@ -8,6 +8,8 @@ const Torneo = () => {
   const [sorteoRealizado, setSorteoRealizado] = useState(false);
   const [resultadosSorteo, setResultadosSorteo] = useState([]);
   const [torneoCreado, setTorneoCreado] = useState(false);
+  const [llavesGeneradas, setLlavesGeneradas] = useState(false);
+  const [llaves, setLlaves] = useState([]);
 
   const handleNombreTorneoChange = (event) => {
     setNombreTorneo(event.target.value);
@@ -25,6 +27,8 @@ const Torneo = () => {
     setTorneoCreado(true);
     setSorteoRealizado(false);
     setResultadosSorteo([]);
+    setLlavesGeneradas(false);
+    setLlaves([]);
   };
 
   const handleSortearEquipos = () => {
@@ -44,75 +48,115 @@ const Torneo = () => {
     }
   };
 
+  const handleSortearLlaves = () => {
+    if (resultadosSorteo.length === 0) {
+      alert('Primero debe realizar el sorteo de equipos.');
+      return;
+    }
+
+    const participantesAleatorios = shuffle([...resultadosSorteo]);
+    const nuevasLlaves = [];
+    for (let i = 0; i < participantesAleatorios.length; i += 2) {
+      if (i + 1 < participantesAleatorios.length) {
+        nuevasLlaves.push({
+          enfrentamiento: `${participantesAleatorios[i].participante}, equipo ${participantesAleatorios[i].equipo} vs ${participantesAleatorios[i + 1].participante}, equipo ${participantesAleatorios[i + 1].equipo}`,
+        });
+      } else {
+        // Participante sin pareja, pasa directamente a la siguiente fase
+        nuevasLlaves.push({
+          enfrentamiento: `${participantesAleatorios[i].participante}, equipo ${participantesAleatorios[i].equipo} (Pasa automáticamente a la siguiente fase)`,
+        });
+      }
+    }
+    setLlaves(nuevasLlaves);
+    setLlavesGeneradas(true);
+  };
+
   return (
     <div>
       <h1>Torneo</h1>
 
-      {!sorteoRealizado && (
-        <section>
-          <h2>Crear Nuevo Torneo</h2>
-          <div>
-            <label>
-              Nombre del Torneo:
-              <input type="text" value={nombreTorneo} onChange={handleNombreTorneoChange} />
-            </label>
-          </div>
-          <div>
-            <button onClick={handleCrearTorneo}>Crear Torneo</button>
-            {torneoCreado && <span> - Torneo creado correctamente</span>}
-          </div>
-        </section>
-      )}
-
       <section>
-        <h2>Sorteo de Equipos</h2>
-        {!sorteoRealizado && (
+        {!sorteoRealizado && !llavesGeneradas && (
           <div>
-            <label>
-              Ingrese los nombres de los participantes (separados por comas):
-              <input type="text" value={participantes} onChange={handleParticipantesChange} />
-            </label>
-          </div>
-        )}
-        {!sorteoRealizado && (
-          <div>
-            <label>
-              Ingrese los nombres de los equipos (separados por comas):
-              <input type="text" value={equipos} onChange={handleEquiposChange} />
-            </label>
-          </div>
-        )}
-        {!sorteoRealizado && (
-          <div>
-            <button onClick={handleSortearEquipos}>Sortear</button>
+            <h2>Crear Nuevo Torneo</h2>
+            <div>
+              <label>
+                Nombre del Torneo:
+                <input type="text" value={nombreTorneo} onChange={handleNombreTorneoChange} />
+              </label>
+            </div>
+            <div>
+              <button onClick={handleCrearTorneo}>Crear Torneo</button>
+              {torneoCreado && <span> - Torneo creado correctamente</span>}
+            </div>
           </div>
         )}
       </section>
 
-      {sorteoRealizado && (
-        <section>
-          <h2>Resultados del Sorteo</h2>
-          <p>Torneo: {nombreTorneo}</p>
-          <table>
-            <thead>
-              <tr>
-                <th>Participante</th>
-                <th>Equipo Asignado</th>
-              </tr>
-            </thead>
-            <tbody>
-              {resultadosSorteo.map((resultado, index) => (
-                <tr key={index}>
-                  <td>{resultado.participante}</td>
-                  <td>{resultado.equipo}</td>
+      <section>
+        {torneoCreado && !sorteoRealizado && !llavesGeneradas && (
+          <div>
+            <h2>Sorteo de Equipos</h2>
+            <div>
+              <label>
+                Ingrese los nombres de los participantes (separados por comas):
+                <input type="text" value={participantes} onChange={handleParticipantesChange} />
+              </label>
+            </div>
+            <div>
+              <label>
+                Ingrese los nombres de los equipos (separados por comas):
+                <input type="text" value={equipos} onChange={handleEquiposChange} />
+              </label>
+            </div>
+            <div>
+              <button onClick={handleSortearEquipos}>Sortear</button>
+            </div>
+          </div>
+        )}
+
+        {sorteoRealizado && !llavesGeneradas && (
+          <div>
+            <h2>Resultados del Sorteo</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th>Participante</th>
+                  <th>Equipo Asignado</th>
                 </tr>
+              </thead>
+              <tbody>
+                {resultadosSorteo.map((resultado, index) => (
+                  <tr key={index}>
+                    <td>{resultado.participante}</td>
+                    <td>{resultado.equipo}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <div>
+              <button onClick={handleSortearLlaves}>Sortear y Mostrar Llaves</button>
+            </div>
+          </div>
+        )}
+      </section>
+
+      <section>
+        {llavesGeneradas && (
+          <div>
+            <h2>Llaves del Torneo</h2>
+            <ul>
+              {llaves.map((llave, index) => (
+                <li key={index}>{llave.enfrentamiento}</li>
               ))}
-            </tbody>
-          </table>
-        </section>
-      )}
+            </ul>
+          </div>
+        )}
+      </section>
     </div>
   );
 };
 
 export default Torneo;
+
