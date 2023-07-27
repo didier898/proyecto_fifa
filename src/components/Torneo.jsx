61b66e4e259435b6d6447ca20b5e8ba7b76f2867
Didier <didier002010@gmail.com>
2023-07-27 17:22:19 -0500

    commit de seccion sortear participantes y equipos

--- src/components/Torneo.jsx
+++ src/components/Torneo.jsx
@@ -1,36 +1,118 @@
 import React, { useState } from 'react';
-
+import { shuffle } from 'lodash';
 
 const Torneo = () => {
-    const [nombreTorneo, setNombreTorneo] = useState('');
-  
-    const handleNombreChange = (event) => {
-      setNombreTorneo(event.target.value);
-    };
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      // Aquí podrías realizar alguna acción con el nombre del torneo, como guardarlo en la base de datos o realizar otros cálculos relacionados con el torneo.
-      alert(`¡Torneo "${nombreTorneo}" creado correctamente!`);
-      setNombreTorneo(''); // Reiniciamos el campo del formulario después de enviarlo.
-    };
-  
-    return (
-      <div>
-        <h1>Torneo</h1>
+  const [nombreTorneo, setNombreTorneo] = useState('');
+  const [participantes, setParticipantes] = useState('');
+  const [equipos, setEquipos] = useState('');
+  const [sorteoRealizado, setSorteoRealizado] = useState(false);
+  const [resultadosSorteo, setResultadosSorteo] = useState([]);
+  const [torneoCreado, setTorneoCreado] = useState(false);
+
+  const handleNombreTorneoChange = (event) => {
+    setNombreTorneo(event.target.value);
+  };
+
+  const handleParticipantesChange = (event) => {
+    setParticipantes(event.target.value);
+  };
+
+  const handleEquiposChange = (event) => {
+    setEquipos(event.target.value);
+  };
+
+  const handleCrearTorneo = () => {
+    setTorneoCreado(true);
+    setSorteoRealizado(false);
+    setResultadosSorteo([]);
+  };
+
+  const handleSortearEquipos = () => {
+    const participantesArray = participantes.split(',').map((participante) => participante.trim());
+    const equiposArray = equipos.split(',').map((equipo) => equipo.trim());
+
+    if (participantesArray.length === equiposArray.length) {
+      const participantesAleatorios = shuffle([...participantesArray]);
+      const resultados = participantesAleatorios.map((participante, index) => ({
+        participante,
+        equipo: equiposArray[index],
+      }));
+      setResultadosSorteo(resultados);
+      setSorteoRealizado(true);
+    } else {
+      alert('Error: La cantidad de participantes debe ser igual a la cantidad de equipos.');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Torneo</h1>
+
+      {!sorteoRealizado && (
         <section>
           <h2>Crear Nuevo Torneo</h2>
-          <form onSubmit={handleSubmit}>
+          <div>
             <label>
               Nombre del Torneo:
-              <input type="text" value={nombreTorneo} onChange={handleNombreChange} />
+              <input type="text" value={nombreTorneo} onChange={handleNombreTorneoChange} />
             </label>
-            <button type="submit">Crear Torneo</button>
-          </form>
+          </div>
+          <div>
+            <button onClick={handleCrearTorneo}>Crear Torneo</button>
+            {torneoCreado && <span> - Torneo creado correctamente</span>}
+          </div>
         </section>
-      </div>
-    );
-  };
-  
-  export default Torneo;
-  
+      )}
+
+      <section>
+        <h2>Sorteo de Equipos</h2>
+        {!sorteoRealizado && (
+          <div>
+            <label>
+              Ingrese los nombres de los participantes (separados por comas):
+              <input type="text" value={participantes} onChange={handleParticipantesChange} />
+            </label>
+          </div>
+        )}
+        {!sorteoRealizado && (
+          <div>
+            <label>
+              Ingrese los nombres de los equipos (separados por comas):
+              <input type="text" value={equipos} onChange={handleEquiposChange} />
+            </label>
+          </div>
+        )}
+        {!sorteoRealizado && (
+          <div>
+            <button onClick={handleSortearEquipos}>Sortear</button>
+          </div>
+        )}
+      </section>
+
+      {sorteoRealizado && (
+        <section>
+          <h2>Resultados del Sorteo</h2>
+          <p>Torneo: {nombreTorneo}</p>
+          <table>
+            <thead>
+              <tr>
+                <th>Participante</th>
+                <th>Equipo Asignado</th>
+              </tr>
+            </thead>
+            <tbody>
+              {resultadosSorteo.map((resultado, index) => (
+                <tr key={index}>
+                  <td>{resultado.participante}</td>
+                  <td>{resultado.equipo}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </section>
+      )}
+    </div>
+  );
+};
+
+export default Torneo;
