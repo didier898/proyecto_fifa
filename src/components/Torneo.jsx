22ff41dd5795d2acd20c5f467aaa9be2c603519c
Didier <didier002010@gmail.com>
2023-07-27 17:09:22 -0500

    commit crear torneo

--- src/components/Torneo.jsx
+++ src/components/Torneo.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react';
+
 
 const Torneo = () => {
-  return (
-    <div>Torneo</div>
-  )
-}
-
-export default Torneo
+    const [nombreTorneo, setNombreTorneo] = useState('');
+  
+    const handleNombreChange = (event) => {
+      setNombreTorneo(event.target.value);
+    };
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      // Aquí podrías realizar alguna acción con el nombre del torneo, como guardarlo en la base de datos o realizar otros cálculos relacionados con el torneo.
+      alert(`¡Torneo "${nombreTorneo}" creado correctamente!`);
+      setNombreTorneo(''); // Reiniciamos el campo del formulario después de enviarlo.
+    };
+  
+    return (
+      <div>
+        <h1>Torneo</h1>
+        <section>
+          <h2>Crear Nuevo Torneo</h2>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Nombre del Torneo:
+              <input type="text" value={nombreTorneo} onChange={handleNombreChange} />
+            </label>
+            <button type="submit">Crear Torneo</button>
+          </form>
+        </section>
+      </div>
+    );
+  };
+  
+  export default Torneo;
+  
